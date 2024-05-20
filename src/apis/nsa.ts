5a5fed5ad5dba4dba5bf09e021e2c46da3f3e55f
jkpark <pjk0007@naver.com>
2024-05-20 13:24:35 +0900

    feat: nds요청 보낼 시 timestamp 통일

--- src/apis/nsa.ts
+++ src/apis/nsa.ts
@@ -1,3 +1,4 @@
+import { time } from 'console';
 import crypto from 'crypto';
 
 const SERVICE_URL = 'https://api.searchad.naver.com';
@@ -5,9 +6,9 @@ const SERVICE_URL = 'https://api.searchad.naver.com';
 export function nsaSign(
   uri: string,
   method: string = 'GET',
-  api_secret: string
+  api_secret: string,
+  timestamp: number
 ) {
-  const timestamp = Date.now();
   const hmac = crypto.createHmac('sha256', api_secret);
   hmac.update(`${timestamp}.${method}.${uri}`);
   return hmac.digest('base64');
@@ -22,9 +23,9 @@ export async function nsaRequest(
   customer_id: number,
   body?: Record<string, any>
 ) {
-  const timestamp = Date.now();
   const urlSearchParams = new URLSearchParams(params).toString();
-  const sign = nsaSign(uri, method, api_secret);
+  const timestamp = Date.now();
+  const sign = nsaSign(uri, method, api_secret, timestamp);
   const headers = {
     'X-Timestamp': `${timestamp}`,
     'X-API-KEY': api_key,
@@ -40,12 +41,13 @@ export async function nsaRequest(
   if (body) {
     options.body = JSON.stringify(body);
   }
+
   const result = await fetch(
     `${SERVICE_URL}${uri}?${urlSearchParams}`,
     options
   );
-  if (!result.ok) {
-    throw new Error(`[${result.status}]: ${result.statusText}`);
-  }
+  // if (!result.ok) {
+  //   throw new Error(`[${result.status}]: ${result.statusText}`);
+  // }
   return result.json();
 }
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import cors from 'cors';
 const app: Express = express();
 const port = 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
